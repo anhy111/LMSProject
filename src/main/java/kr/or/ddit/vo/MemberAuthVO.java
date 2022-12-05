de36a3174b3c20f192b9e8cbbba1dc7254b16456
HISeungYeon <sy.lee5081@gmail.com>
2022-12-05 14:29:20 +0900

    vo커밋

---
+++ src/main/java/kr/or/ddit/vo/MemberAuthVO.java
@@ -0,0 +1,32 @@
+package kr.or.ddit.vo;
+
+public class MemberAuthVO {
+	private int memNo;
+	private String auth;
+	
+	public MemberAuthVO() {}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberAuthVO [memNo=" + memNo + ", auth=" + auth + "]";
+	}
+
+	
+	
+}
