bf14a7183f3244635afcaca50a43e37119b1c5fb
seongbo <shimseongbo>
2022-12-05 12:22:47 +0900

    Dec05 - seongbo

--- src/main/java/kr/or/ddit/test/testCont.java
+++ src/main/java/kr/or/ddit/test/testCont.java
@@ -1,15 +1,27 @@
 package kr.or.ddit.test;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+
 
 @Controller
 public class testCont {
-	
+
+
 	@GetMapping("/test/home")
 	public String test() {
+
 		return "test/home";
 	}
-	
 
+	@GetMapping("/th")
+	public String testThymeleaf(Model model) {
+		model.addAttribute("data", "data");
+		return "action2";
+	}
 }
