a83b5e8f1bd4bc28e4feda3114973820a1ffea3a
abled <intensity23>
2022-12-05 16:32:40 +0900

    vo몇 개 추가

---
+++ src/main/java/kr/or/ddit/domain/Employee.java
@@ -0,0 +1,25 @@
+package kr.or.ddit.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+	private int empNo; //직원번호
+	private String empNm;	//한글 이름	
+	private String empNme;	//영문 이름
+	private String empTel;	//전화번호
+	private String empTel2; //비상연락처
+	private int memZip;		//우편번호
+	private String empAddr1;//기본주소
+	private String empAddr2;//상세주소
+	private String empReg;	//주민번호
+	private String empBankCd;//은행코드
+	private String empDepo;	//예금주	
+	private String empAct;	//급여지급 계좌
+	private String empPic;	//증명사진 URL
+	private Date empJoin;	//입사일
+	private Date empRet;	//퇴사일
+}
---
+++ src/main/java/kr/or/ddit/domain/Professor.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.domain;
+
+import lombok.Data;
+
+@Data
+public class Professor {
+	
+	private int proNo;//교수번호
+	private int depCd;//학과코드
+	private String proPos;//직책명
+
+}
