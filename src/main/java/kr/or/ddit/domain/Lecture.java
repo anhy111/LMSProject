ad74506850432e137f846268cd37675f08f0cd74
abled <intensity23>
2022-12-05 21:08:18 +0900

    승인여부 int에서 string으로 바꿈

--- src/main/java/kr/or/ddit/domain/Lecture.java
+++ src/main/java/kr/or/ddit/domain/Lecture.java
@@ -7,8 +7,8 @@ public class Lecture {
 
 	private int lecaCd;// 강의코드
 	private int subCd;// 과목코드
-	private int lecYn;// 개강 상태
+	private String lecYn;// 개강 상태
 	private int lecHcnt;// 수강인원
-	private int lecaYn;// 승인여부
+	private String lecaYn;// 승인여부
 	private int depCd;// 학과코드
 }
