43253ec283a0c38807b163e4b3044b354c93ec61
abled <intensity23>
2022-12-06 21:11:49 +0900

    디파트먼트

---
+++ src/main/java/kr/or/ddit/domain/Department.java
@@ -0,0 +1,17 @@
+package kr.or.ddit.domain;
+
+import lombok.Data;
+
+@Data
+public class Department {
+
+	private int depCd ;
+	private int colCd ;
+	private int proNo ;
+	private String depNm ;
+	private String depDes;
+	private int depCap;
+	private String depTel;
+	private int empNo ;
+	
+}
