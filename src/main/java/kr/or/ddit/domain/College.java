bdcfe0c4de6ead30f2ffb29d172abfeb7959e055
loy18 <yeonsker@gmail.com>
2022-12-06 14:13:14 +0900

    단과대 업뎃

--- src/main/java/kr/or/ddit/domain/College.java
+++ src/main/java/kr/or/ddit/domain/College.java
@@ -4,7 +4,18 @@ import lombok.Data;
 
 @Data
 public class College {
-
-	private int colCd; //단과대 코드
-	private String colNm; // 단과대학명
+	
+	
+	private int colCd;	// 단과대코드
+	private String colNm; //단과대학명
+	private String colDes; //단과대설명
+	private String colTel; //단과대연락처
+	private String colAddr; //단과대위치
+	private String colImgUrl; //단과대이미지
+	
+	public College() {
+	}
+	
+	
+	
 }
