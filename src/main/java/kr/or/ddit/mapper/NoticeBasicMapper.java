2165bac1167f586bb5a008af05c0577c90fd4170
shimseongbo <shimseongbo>
2022-12-11 00:05:53 +0900

    공지사항 등록기능 추가

--- src/main/java/kr/or/ddit/mapper/NoticeBasicMapper.java
+++ src/main/java/kr/or/ddit/mapper/NoticeBasicMapper.java
@@ -6,13 +6,21 @@ import java.util.List;
 
 public interface NoticeBasicMapper {
 
+
+    //공지사항 목록 조회
+    List<NoticeBasic> noticeBasicList();
+
+    //공지사항 상세 조회
+    NoticeBasic noticeBasicFindNotice(Long noticeCd);
+
     //공지사항 저장
     void noticeBasicSave(NoticeBasic noticeBasic);
 
-    //공지사항 목록
-    List<NoticeBasic> noticeBasicList();
+    //공지사항 수정
+    void noticeBasicUpdate(NoticeBasic noticeBasic);
 
-    NoticeBasic noticeBasicFindNotice(Long noticeCd);
-
+    //공지사항 삭제
     void noticeBasicDelete(Long noticeCd);
+
+
 }
