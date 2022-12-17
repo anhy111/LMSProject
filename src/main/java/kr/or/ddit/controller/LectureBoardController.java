c6e75a56e56cc8e4c4852410d220f28be0d18d2d
abled <intensity23>
2022-12-17 15:11:37 +0900

    컷밋~

--- src/main/java/kr/or/ddit/controller/LectureBoardController.java
+++ src/main/java/kr/or/ddit/controller/LectureBoardController.java
@@ -1,6 +1,7 @@
 package kr.or.ddit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,5 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/lectureBoard")
 public class LectureBoardController {
 	
+	
+	@GetMapping("/data/lectureData")
+	public String lectureMaterial() {
+		return "lectureBoard/data/lectureData";
+	}
 
 }
