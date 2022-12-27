abcd56aa131c36fd8a5cf300b753508f9f4d4a6c
shimseongbo <seong265@icloud.com>
2022-12-27 10:56:57 +0900

    컨트롤러 추가

---
+++ src/main/java/kr/or/ddit/controller/BambooBoardController.java
@@ -0,0 +1,14 @@
+package kr.or.ddit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/bamboo")
+public class BambooBoardController {
+
+    public String bambooBoard() {
+
+
+
+        return "";
+    }
+}
