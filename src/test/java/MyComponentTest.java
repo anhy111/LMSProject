868929e8faeb2977b395e709bcaa8d7f7012e1fd
shimseongbo <shimseongbo>
2023-01-05 18:55:42 +0900

    aws / 처무파일 공지사항

--- src/test/java/MyComponentTest.java
+++ src/test/java/MyComponentTest.java
@@ -1,21 +1,30 @@
-//import kr.or.ddit.aws.MyComponent;
-//import kr.or.ddit.aws.S3Config;
-//import org.junit.Test;
-//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-//import org.springframework.context.annotation.ComponentScan;
-//
-//@ComponentScan
-//public class MyComponentTest {
-//
-//    @Test
-//    public void s3ClientTest() {
-//
-//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-//        if (!context.isActive()) {
-//            context.register(S3Config.class);
-//            context.refresh();
-//        }
-//        MyComponent myComponent = context.getBean(MyComponent.class);
-//        myComponent.doSomething();
-//    }
-//}
+import kr.or.ddit.aws.MyComponent;
+import kr.or.ddit.aws.S3Config;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class MyComponentTest {
+
+    @Autowired
+    S3Config s3Config;
+
+    @Autowired
+    MyComponent myComponent;
+
+    @Test
+    public void s3ClientTest() {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        if (!context.isActive()) {
+            context.register(S3Config.class);
+            context.refresh();
+        }
+        MyComponent myComponent = context.getBean(MyComponent.class);
+        myComponent.doSomething();
+
+    }
+}
