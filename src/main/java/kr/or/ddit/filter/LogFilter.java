a47392e042d283cdab1fdf953961dfdc0453b450
shimseongbo <shimseongbo>
2023-01-06 19:11:57 +0900

    공지사항 필터빼고

--- src/main/java/kr/or/ddit/filter/LogFilter.java
+++ src/main/java/kr/or/ddit/filter/LogFilter.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.security.Principal;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class LogFilter implements Filter {
@@ -17,26 +20,37 @@ public class LogFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
         log.info("log filter doFilter");
-
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        long time = System.currentTimeMillis();
+        System.out.println("time = " + time);
+
+        String userId = httpRequest.getRemoteUser();
+        System.out.println("userId = " + userId);
+
         String requestURI = httpRequest.getRequestURI();
+        System.out.println("requestURI = " + requestURI);
 
-        String uuid = UUID.randomUUID().toString();
+        String regex = "^/qna/(\\d+)$";
+        // 정규표현식을 컴파일한다
+        Pattern pattern = Pattern.compile(regex);
+        System.out.println("pattern = " + pattern);
 
-        try {
-            log.info("Request [{}][{}]", uuid, requestURI);
-            filterChain.doFilter(servletRequest, servletResponse);
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            log.info("Response [{}][{}]", uuid, requestURI);
+        // 입력 문자열에 정규표현식을 적용한다
+        Matcher matcher = pattern.matcher(requestURI);
+        System.out.println("matcher = " + matcher);
+
+        // 정규표현식과 일치하는 문자열을 찾는다
+        if (matcher.find()) {
+            // 캡쳐한 그룹(group)을 추출한다
+            String group = matcher.group(1);
         }
     }
 
     @Override
     public void destroy() {
+
         log.info("log filter destroy");
     }
 }
