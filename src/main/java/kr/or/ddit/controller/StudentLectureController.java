21c4012c872660181c5f1251f73bae99dfa5d086
Anhy <Anhy@DESKTOP-2451KGF>
2023-01-10 21:27:33 +0900

    ???

--- src/main/java/kr/or/ddit/controller/StudentLectureController.java
+++ src/main/java/kr/or/ddit/controller/StudentLectureController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -81,4 +82,12 @@ public class StudentLectureController {
 		model.addAttribute("data", lecApplySearch);
 		return "lectureBoard/score/totalScore2";
 	}
+	
+	@ResponseBody
+	@GetMapping("/lectureCount") 
+	public String lectureCount(StudentLecture studentLecture) {
+		log.info("StudentLectureController.lectureCount.studentLecture : " + studentLecture);
+		return this.lectureService.lectureListPaging(studentLecture) + "";
+	}
+	
 }
